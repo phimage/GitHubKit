74c97f3cbd6c6b069fc3ee81aaf9e6f9befb6047
mesopelagique <eric.marchand@4d.com>
2020-07-26 23:24:17 +0200

    Make github public to be able to create missing request

--- Sources/GitHubKit/Query.swift
+++ Sources/GitHubKit/Query.swift
@@ -6,7 +6,6 @@ public struct QueryableProperty<QueryableType> {
     /// Queryable element accessor
     public var element: QueryableType?
     
-    let github: GitHubClient
     
     init(_ obj: QueryableType? = nil, github: GitHubClient) {
         element = obj
